f648af047f3230a0aa540cb6ba4ca19632feaa63
thiagocolen <thiago.souzacolen@gmail.com>
2018-03-17 12:14:42 -0300

    creating a store and a reducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
 import './App.css'
+
+
+let store = createStore(rootReducer)
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>asd</h1>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>asd</h1>
+        </div>
+      </Provider>
     );
   }
 }
 
-export default App;
+
+export default App
---
+++ src/reducers/contacts.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {}
+
+const contacts = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ACTION_TESTE':
+      return [
+        ...state
+      ]
+    default:
+      return state
+  }
+}
+ 
+export default contacts
